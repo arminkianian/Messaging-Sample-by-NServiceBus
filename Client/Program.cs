1261a56c6c8f62d91218d7d346d80884d8109dff
ArminKianian <arminkia.dev@gmail.com>
2023-09-07 13:14:06 +0430

    RabbitMQ Transport

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Messages;
+using NServiceBus;
 
 Console.WriteLine("Hello, World!");
-
 Console.Title = "Client";
+
 var configuration = new EndpointConfiguration("Client");
-configuration.UseTransport<LearningTransport>();
+configuration.SendFailedMessagesTo("Client.Error");
+configuration.EnableInstallers();
+
+var transport = configuration.UseTransport<RabbitMQTransport>();
+transport.UseConventionalRoutingTopology(
+    queueType: QueueType.Classic // Specify the queueType here
+);
+transport.ConnectionString("host=localhost");
 
 var endpointInstance = Endpoint.Start(configuration).Result;
 
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,7 +3,14 @@ Console.WriteLine("Hello, World!");
 
 Console.Title = "Server";
 var configuration = new EndpointConfiguration("Server");
-configuration.UseTransport<LearningTransport>();
+configuration.SendFailedMessagesTo("Server.Error");
+configuration.EnableInstallers();
+
+var transport = configuration.UseTransport<RabbitMQTransport>();
+transport.UseConventionalRoutingTopology(
+    queueType: QueueType.Classic // Specify the queueType here
+);
+transport.ConnectionString("host=localhost");
 
 var endpointInstance = Endpoint.Start(configuration).Result;
 
